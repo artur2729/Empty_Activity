d49e36c8725f0b9a01fe461d50ff4681c6ff83dc
Artur <a.tymulis@gmail.com>
2023-03-21 14:34:27 +0200

    Call finish function from clockListener

--- app/src/main/java/lt/arturas/emptyactivity/SecondActivity.kt
+++ app/src/main/java/lt/arturas/emptyactivity/SecondActivity.kt
@@ -1,5 +1,6 @@
 package lt.arturas.emptyactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,13 +12,15 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
 
-        val myTextView : TextView = findViewById(R.id.myTextViewSecond)
+        val myTextView: TextView = findViewById(R.id.myTextViewSecond)
         val closeButton: Button = findViewById(R.id.closeButton)
 
         myTextView.setText(R.string.SecondAtivity_text_view)
         closeButton.setText(R.string.SecondAtivity_button_close)
 
+        closeButton.setOnClickListener {
+            //Log.i(TAG, "onCreate: just clicked OPEN button")
+            finish()
+        }
     }
-
-
 }
