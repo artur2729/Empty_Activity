b09a51d9982ac633822f0d698733dea9e0d7f9b3
Artur <a.tymulis@gmail.com>
2023-03-21 13:54:34 +0200

    Second Activity created textveiw and button

--- app/src/main/java/lt/arturas/emptyactivity/SecondActivity.kt
+++ app/src/main/java/lt/arturas/emptyactivity/SecondActivity.kt
@@ -2,10 +2,19 @@ package lt.arturas.emptyactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        val myTextView : TextView = findViewById(R.id.myTextViewSecond)
+        val closeButton: Button = findViewById(R.id.closeButton)
+
     }
+
+
 }
